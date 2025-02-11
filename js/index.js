7598ed4966a1f6a9d6a8159aaf628c833f069acd
Eder Nuñez <santieder16@gmail.com>
2025-02-11 16:55:39 -0500

    fetch json api

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vRjtGEeJcNRGooq5bwqKnXKlRtJtXxQjDC2iyTMC9onSAjhC53z2C9MNiLF1GekE-0oOY1icBC7QvyP/pub?output=csv')
-    .then(response => response.text())
-    .then(data => {
-        const rows = data.split("\n").map(row => row.split(","));
-        console.log(rows); // Muestra los datos en la consola
-    })
+fetch('https://script.googleusercontent.com/macros/echo?user_content_key=41JsTHfCjNoWgRLT6dgILMdvnbtsxG4_oBbSToAEIK8C9DppqU45A4XJN6fmwzj_2CZ4G3a_PuobWBjSIxAiFDmz9k48Mrf1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHFxJsOqqGTxRUD-IqJDWsKthlK0Zwyyj-sMsVGCwINvsuheg0VPbbr_Z05QLT0DfjV8fYiwH4UPf8PMv2hopZvuLZNfWSO_Jdz9Jw9Md8uu&lib=MZj_kfHkoxrRpYNE8CWpKtGJUPzYgWWy8')
+    .then(response => response.json())
+    .then(response => console.log(response))
     .catch(error => console.error("Error al obtener los datos:", error));
