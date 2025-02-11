462d32a06e152103921b1112c582858e09743489
Eder Nuñez <santieder16@gmail.com>
2025-02-11 14:15:33 -0500

    fetch api sheets

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+const ID_HOJA_CALCULO = "1pnbcy5oBQNNAyWMrmgmkiisC02olukoot_FlXTJgXcQ"
+const API_KEY = "AIzaSyA_00hUBLJ1hE9E3Cs8JiaKoqdN0rjtsBc";
+const values = 'A2:AZ100';
 
+const obj = {
+    data: {
+        listaDatos: []
+    }
+}
+
+fetch(`https://content-sheets.googleapis.com/v4/spreadsheets/${ID_HOJA_CALCULO}/values/${values}?access_token=${API_KEY}&key=${API_KEY}`)
+    .then(lista => lista.json())
+    .then(valores => {
+        obj.listaDatos = valores.values;
+    }).catch(err => console.log(err));
